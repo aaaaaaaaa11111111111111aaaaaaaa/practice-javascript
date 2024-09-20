111c62f2374d75acaf5f969723c19b00cf11176a
aaaaaaaaa11111111111111aaaaaaaa <sakaguchioosuke@gmail.com>
2024-09-20 04:49:57 +0000

    ９月18日分を別ファイルに移動

---
+++ 9_19/main.js
@@ -0,0 +1,19 @@
+document.querySelector("h1").textContent="自分の名前"
+document.querySelector("h2").textContent="sakaguchi"
+
+//documentとは！！
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM(ドむ)
+/*ドキュメントオブジェクトモデル(Document Object)の略で
+Webページの要素やコンテンツなどをツリー構造で表現したデータモデル
+*/
+
+//上記コードの意味
+//Document.queryselector("h1").textContent="自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//Document.queryselector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent
